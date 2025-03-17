a62cb1d5d0ba564070ebdbe6c51b421eebc3e64b
GlitchAnims <theglitchanims@gmail.com>
2025-03-17 11:52:52 -0300

    Just kidding, now it's fixed

--- ModularSkillScripts/ModularScripts.cs
+++ ModularSkillScripts/ModularScripts.cs
@@ -1891,25 +1891,26 @@ namespace ModularSkillScripts
 				case "getstat":{
 					BattleUnitModel targetModel = GetTargetModel(circles[0]);
 					if (targetModel == null) return;
+					int value = -1;
 
 					string circle_1 = circles[1];
 					if (circle_1 == "deployment") value = targetModel.PARTICIPATE_ORDER;
 					else if (circle_1 == "deadAllyCount") value = targetModel.deadAllyCount;
 					else if (circle_1 == "panicType") value = Convert.ToInt32(targetModel._defaultPanicType);
 					else if (circle_1 == "isRetreated") value = targetModel.IsRetreated() ? 1 : 0;
-					else if (circle_1.StartsWith("res"))
-					{
+					else if (circle_1.StartsWith("res")) {
 						string word = circle_1.Remove(0, 3);
 						ATK_BEHAVIOUR atk = ATK_BEHAVIOUR.NONE;
 						Enum.TryParse(word, true, out atk);
 						if (atk != ATK_BEHAVIOUR.NONE) {
-							valueList[setvalue_idx] = (int)(targetModel.GetAtkResistMultiplier(atk) * 100.0f);
-							return;
+							value = (int)(targetModel.GetAtkResistMultiplier(atk) * 100.0f);
 						}
-
-						ATTRIBUTE_TYPE sin = ATTRIBUTE_TYPE.NONE;
-						Enum.TryParse(word, true, out sin);
-						if (sin != ATTRIBUTE_TYPE.NONE) valueList[setvalue_idx] = (int)(targetModel.GetAttributeResistMultiplier(sin) * 100.0f);
+						else {
+							ATTRIBUTE_TYPE sin = ATTRIBUTE_TYPE.NONE;
+							Enum.TryParse(word, true, out sin);
+							if (sin != ATTRIBUTE_TYPE.NONE) value = (int)(targetModel.GetAttributeResistMultiplier(sin) * 100.0f);
+						}
+						valueList[setvalue_idx] = value;
 					}
 				}
 				break;
