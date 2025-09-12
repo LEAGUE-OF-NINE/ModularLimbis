5f0f060f75e65650f13685926eed146382673561
pufferffish <me@windtfw.com>
2025-09-12 20:48:41 +0200

    make second argument in log consequence unnecessary

--- ModularSkillScripts/Consequence/ConsequenceLog.cs
+++ ModularSkillScripts/Consequence/ConsequenceLog.cs
@@ -4,6 +4,17 @@ public class ConsequenceLog : IModularConsequence
 {
 	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
 	{
-		MainClass.Logg.LogInfo("ModularLog " + circles[0] + ": " + modular.GetNumFromParamString(circles[1]));
+		switch (circles.Length)
+		{
+			case 0:
+				MainClass.Logg.LogInfo("ModularLog");
+				break;
+			case 1:
+				MainClass.Logg.LogInfo("ModularLog " + circles[0]);
+				break;
+			default:
+				MainClass.Logg.LogInfo("ModularLog " + circles[0] + ": " + modular.GetNumFromParamString(circles[1]));
+				break;
+		}
 	}
 }
