a6150f5b1a5987906221ed3270edaadd090d134f
dervorce <derdbolt@gmail.com>
2025-09-16 14:01:59 +0300

    version pmump i forgot to do

--- ModularSkillScripts/MainClass.cs
+++ ModularSkillScripts/MainClass.cs
@@ -259,7 +259,7 @@ public class MainClass : BasePlugin
 	public static bool logEnabled = false;
 
 	public const string NAME = "ModularSkillScripts";
-	public const string VERSION = "4.0.0";
+	public const string VERSION = "4.1.0";
 	public const string AUTHOR = "GlitchGames";
 	public const string GUID = $"{AUTHOR}.{NAME}";
 
