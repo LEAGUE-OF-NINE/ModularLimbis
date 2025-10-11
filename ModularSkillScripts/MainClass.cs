335734b420fe227af45664ab28ce5494a17b370c
whyvl <me@windtfw.com>
2025-10-11 23:11:56 +0100

    bump version

--- ModularSkillScripts/MainClass.cs
+++ ModularSkillScripts/MainClass.cs
@@ -280,7 +280,7 @@ public class MainClass : BasePlugin
 	public static bool logEnabled = false;
 
 	public const string NAME = "ModularSkillScripts";
-	public const string VERSION = "4.2.5";
+	public const string VERSION = "4.3.0";
 	public const string AUTHOR = "GlitchGames";
 	public const string GUID = $"{AUTHOR}.{NAME}";
 
