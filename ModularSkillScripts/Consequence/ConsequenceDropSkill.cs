c3f69ddfb7d93d58086b2b98e151340886d4ce9c
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-03-22 16:05:32 -0300

    Fixed dropskill for the final time

--- ModularSkillScripts/Consequence/ConsequenceDropSkill.cs
+++ ModularSkillScripts/Consequence/ConsequenceDropSkill.cs
@@ -7,15 +7,18 @@ public class ConsequenceDropSkill : IModularConsequence
 		int skillID = modular.GetNumFromParamString(circles[0]);
 		BattleUnitModel unit = modular.modsa_unitModel;
 		BattleActionModel action = modular.modsa_selfAction;
-		if (action != null)
+		if (action != null && circles.Length < 2)
 		{
-			if (circles.Length < 2) action.SinAction.ReplaceReadySkillAtoB(skillID);
-			else
-			{
-				int slotID = modular.GetNumFromParamString(circles[1]);
-				unit.ReplaceReadySkillAtoB(skillID, -1, slotID);
-			}
+			action.SinAction.ReplaceReadySkillAtoB(skillID);
+			return;
 		}
+		
+		if (circles.Length > 1)
+		{
+			int slotID = modular.GetNumFromParamString(circles[1]);
+			unit.ReplaceReadySkillAtoB(skillID, -1, slotID);
+		}
+		
 		//unit.AddNewSinActionModelWithSkill(skillID);
 	} // END ExecuteConsequence
 } // END IModularConsequence
