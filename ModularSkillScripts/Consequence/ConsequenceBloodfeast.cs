24dfc2626825fc4b1fad3c9f21fa2627ed861a46
Not-Nova-Shane <ezpz12453@gmail.com>
2026-04-16 16:29:25 -0400

    a fix fo rnew

--- ModularSkillScripts/Consequence/ConsequenceBloodfeast.cs
+++ ModularSkillScripts/Consequence/ConsequenceBloodfeast.cs
@@ -7,7 +7,7 @@ public class ConsequenceBloodfeast : IModularConsequence
 	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
 	{
 		int bloodDinner = modular.GetNumFromParamString(circles[1]);
-		if (string.Equals(circles[0], "add", StringComparison.OrdinalIgnoreCase)) BloodDinnerBuff.BuffInstance.AddStack(bloodDinner, modular.battleTiming);
+		if (string.Equals(circles[0], "add", StringComparison.OrdinalIgnoreCase)) BloodDinnerBuff.BuffInstance.AddStack(bloodDinner, modular.battleTiming, false);
 		else if (string.Equals(circles[0], "sub", StringComparison.OrdinalIgnoreCase)) BloodDinnerBuff.BuffInstance.SubStack(bloodDinner, modular.battleTiming);
 		else if (circles.Length > 1 && string.Equals(circles[0], "use", StringComparison.OrdinalIgnoreCase))
 		{
