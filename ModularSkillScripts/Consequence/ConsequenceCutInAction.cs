a0bde745185f92d476dbb9882e304fb1b08a2474
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-05-14 23:51:12 -0300

    Testing cutinaction consq, veersion bump

---
+++ ModularSkillScripts/Consequence/ConsequenceCutInAction.cs
@@ -0,0 +1,17 @@
+using System;
+using Il2CppSystem.Collections.Generic;
+
+namespace ModularSkillScripts.Consequence;
+
+public class ConsequenceCutInAction : IModularConsequence
+{
+	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
+	{
+		BattleActionModel action = modular.modsa_selfAction;
+		if (action == null) return;
+		BattleUnitModel unit = action.Model;
+		if (unit == null) return;
+		unit.CutInAction(action);
+		//action.OnOrderCutInAction(modular.modsa_selfAction);
+	}
+}
--- ModularSkillScripts/MainClass.cs
+++ ModularSkillScripts/MainClass.cs
@@ -231,6 +231,7 @@ public class MainClass : BasePlugin
 		consequenceDict["explosion"] = new ConsequenceExplosion();
 		consequenceDict["surge"] = new ConsequenceSurge();
 		consequenceDict["bufcategory"] = new ConsequenceBuffCategory();
+		consequenceDict["cutinaction"] = new ConsequenceCutInAction();
 
 	}
 
@@ -443,7 +444,7 @@ public class MainClass : BasePlugin
 	public static bool logEnabled = false; // for useless logs
 
 	public const string NAME = "ModularSkillScripts";
-	public const string VERSION = "4.7.5";
+	public const string VERSION = "4.7.6";
 	public const string AUTHOR = "GlitchGames";
 	public const string GUID = $"{AUTHOR}.{NAME}";
 
