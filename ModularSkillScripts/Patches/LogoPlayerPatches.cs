632eb2cb6c6685f5ab1309e5c2d9f63b6b93cf05
R894 <R894@users.noreply.github.com>
2026-06-13 08:58:15 +0300

    disable logo player patch for now

--- ModularSkillScripts/Patches/LogoPlayerPatches.cs
+++ ModularSkillScripts/Patches/LogoPlayerPatches.cs
@@ -3,15 +3,15 @@ using HarmonyLib;
 
 namespace ModularSkillScripts.Patches;
 
-internal class LogoPlayerPatches
-{
+// internal class LogoPlayerPatches
+// {
 
-	[HarmonyPatch(typeof(LogoSceneManager), nameof(LogoSceneManager.Awake))]
-	[HarmonyPrefix]
-	private static void Prefix_NewOperationController_EquipDefense()
-	{
-		MainClass.LogModular($"{MainClass.consequenceDict.Count} consequences registered: {string.Join(", ", MainClass.consequenceDict.Keys)}");
-		MainClass.LogModular($"{MainClass.acquirerDict.Count} acquirers registered: {string.Join(", ", MainClass.acquirerDict.Keys)}");
-	}
+// 	[HarmonyPatch(typeof(LogoPlayer), nameof(LogoPlayer.Awake))]
+// 	[HarmonyPrefix]
+// 	private static void Prefix_NewOperationController_EquipDefense()
+// 	{
+// 		MainClass.LogModular($"{MainClass.consequenceDict.Count} consequences registered: {string.Join(", ", MainClass.consequenceDict.Keys)}");
+// 		MainClass.LogModular($"{MainClass.acquirerDict.Count} acquirers registered: {string.Join(", ", MainClass.acquirerDict.Keys)}");
+// 	}
 	
-}
+// }
