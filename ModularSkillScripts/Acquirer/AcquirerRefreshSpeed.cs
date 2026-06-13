6ac64510b72e134c0dad267d1c9fea6a2306ba00
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-06-13 19:09:23 -0300

    indentation at AcquirerRefreshSpeed.cs

--- ModularSkillScripts/Acquirer/AcquirerRefreshSpeed.cs
+++ ModularSkillScripts/Acquirer/AcquirerRefreshSpeed.cs
@@ -2,14 +2,14 @@ namespace ModularSkillScripts.Acquirer;
 
 public class ConsequenceRefreshSpeed : IModularConsequence
 {
-    public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
-    {
-        var modelList = modular.GetTargetModelList(circles[0]);
-        if (modelList.Count < 1) return;
-
-        foreach (BattleUnitModel targetModel in modelList)
-        {
-            targetModel.RefreshSpeed();
-        }
-    }
+	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
+	{
+		var modelList = modular.GetTargetModelList(circles[0]);
+		if (modelList.Count < 1) return;
+		
+		foreach (BattleUnitModel targetModel in modelList)
+		{
+			targetModel.RefreshSpeed();
+		}
+	}
 }
