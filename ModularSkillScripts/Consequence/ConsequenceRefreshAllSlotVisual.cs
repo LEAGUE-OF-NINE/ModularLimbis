aeb5cabdf6d0bbde005608dd36d2adcb5519a55e
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-06-22 11:53:53 -0300

    Finished up refreshallslotvisual

--- ModularSkillScripts/Consequence/ConsequenceRefreshAllSlotVisual.cs
+++ ModularSkillScripts/Consequence/ConsequenceRefreshAllSlotVisual.cs
@@ -22,16 +22,25 @@ public class ConsequenceRefreshAllSlotVisual : IModularConsequence
 			MainClass.LogModular("Dude This NewOperationController shit is FUCKING NULL");
 			return;
 		}
-		
+		//BattleObjectManager objManager = SingletonBehavior<BattleObjectManager>.Instance;
+		//SinManager sinManager_inst = Singleton<SinManager>.Instance;
 		foreach (NewOperationSinActionSlot sinActionSlot in opController.SinActionSlotList)
 		{
 			NewOperationReadySinSlot opReadySinSlot = sinActionSlot._readySinSlot;
+			if (opReadySinSlot && opReadySinSlot._skillSlot && opReadySinSlot.UnitSin != null)
+			{
+				opReadySinSlot._skillSlot.SetSkill(opReadySinSlot.UnitSin, opReadySinSlot.UnitSin.SinOption == SIN_OPTION.Ego);
+			}
 			NewOperationSinSlot opSinSlot_top = sinActionSlot._secondSinSlot;
+			if (opSinSlot_top && opSinSlot_top._skillSlot && opSinSlot_top.UnitSin != null)
+			{
+				opSinSlot_top._skillSlot.SetSkill(opSinSlot_top.UnitSin, opSinSlot_top.UnitSin.SinOption == SIN_OPTION.Ego);
+			}
 			NewOperationSinSlot opSinSlot_bottom = sinActionSlot._firstSinSlot;
-			
-			opReadySinSlot._skillSlot.SetSkill(opReadySinSlot.UnitSin, opReadySinSlot.UnitSin.SinOption == SIN_OPTION.Ego);
-			opSinSlot_top._skillSlot.SetSkill(opSinSlot_top.UnitSin, opSinSlot_top.UnitSin.SinOption == SIN_OPTION.Ego);
-			opSinSlot_bottom._skillSlot.SetSkill(opSinSlot_bottom.UnitSin, opSinSlot_bottom.UnitSin.SinOption == SIN_OPTION.Ego);
+			if (opSinSlot_bottom && opSinSlot_bottom._skillSlot && opSinSlot_bottom.UnitSin != null)
+			{
+				opSinSlot_bottom._skillSlot.SetSkill(opSinSlot_bottom.UnitSin, opSinSlot_bottom.UnitSin.SinOption == SIN_OPTION.Ego);
+			}
 		}
 		
 	}
