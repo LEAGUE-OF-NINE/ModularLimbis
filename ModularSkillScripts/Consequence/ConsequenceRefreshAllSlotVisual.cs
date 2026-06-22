769686bd184b44bfd6fe4cb86bc809fcf73c90c6
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-06-22 02:25:41 -0300

    Trying out changes

--- ModularSkillScripts/Consequence/ConsequenceRefreshAllSlotVisual.cs
+++ ModularSkillScripts/Consequence/ConsequenceRefreshAllSlotVisual.cs
@@ -1,4 +1,7 @@
 using System;
+using BattleUI;
+using BattleUI.Abnormality;
+using BattleUI.BattleUnit;
 using BattleUI.Operation;
 
 namespace ModularSkillScripts.Consequence;
@@ -7,16 +10,28 @@ public class ConsequenceRefreshAllSlotVisual : IModularConsequence
 {
 	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
 	{
-		NewOperationController opController = Singleton<NewOperationController>.Instance;
+		BattleUIRoot battleUIRoot = SingletonBehavior<BattleUIRoot>.Instance;
+		if (!battleUIRoot)
+		{
+			MainClass.LogModular("Dude This BattleUIRoot shit is FUCKING NULL");
+			return;
+		}
+		NewOperationController opController = battleUIRoot.NewOperationController;
 		if (!opController)
 		{
 			MainClass.LogModular("Dude This NewOperationController shit is FUCKING NULL");
 			return;
 		}
-
+		
 		foreach (NewOperationSinActionSlot sinActionSlot in opController.SinActionSlotList)
 		{
-			sinActionSlot.UpdateStateForAb();
+			NewOperationReadySinSlot opReadySinSlot = sinActionSlot._readySinSlot;
+			NewOperationSinSlot opSinSlot_top = sinActionSlot._secondSinSlot;
+			NewOperationSinSlot opSinSlot_bottom = sinActionSlot._firstSinSlot;
+			
+			opReadySinSlot._skillSlot.SetSkill(opReadySinSlot.UnitSin, opReadySinSlot.UnitSin.SinOption == SIN_OPTION.Ego);
+			opSinSlot_top._skillSlot.SetSkill(opSinSlot_top.UnitSin, opSinSlot_top.UnitSin.SinOption == SIN_OPTION.Ego);
+			opSinSlot_bottom._skillSlot.SetSkill(opSinSlot_bottom.UnitSin, opSinSlot_bottom.UnitSin.SinOption == SIN_OPTION.Ego);
 		}
 		
 	}
