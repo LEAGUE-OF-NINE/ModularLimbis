88a846b076758ae5bef23b792f693f3e89e9cacc
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-07-06 14:51:38 -0300

    Improved circle parsing on skilldeftype

--- ModularSkillScripts/Acquirer/AcquirerSkillDefType.cs
+++ ModularSkillScripts/Acquirer/AcquirerSkillDefType.cs
@@ -4,9 +4,9 @@ public class AcquirerSkillDefType : IModularAcquirer
 {
 	public int ExecuteAcquirer(ModularSA modular, string section, string circledSection, string[] circles)
 	{
-		BattleActionModel action = modular.modsa_selfAction;
-		if (circles[0] == "Target") action = modular.modsa_oppoAction;
-		if (action != null) return (int)action.Skill.GetDefenseType();
+		SkillModel skill = modular.modsa_skillModel;
+		if (circles.Length > 0 && circles[0] == "Target") skill = modular.modsa_oppoAction?.Skill;
+		if (skill != null) return (int)skill.GetDefenseType();
 		return -1;
 	}
 }
