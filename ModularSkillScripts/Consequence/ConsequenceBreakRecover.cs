f05ce97d2dc110c2b8acb7516ab77baf5af36813
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-07-26 15:58:35 -0300

    Il2cpp List

--- ModularSkillScripts/Consequence/ConsequenceBreakRecover.cs
+++ ModularSkillScripts/Consequence/ConsequenceBreakRecover.cs
@@ -1,10 +1,12 @@
+using Il2CppSystem.Collections.Generic;
+
 namespace ModularSkillScripts.Consequence;
 
 public class ConsequenceBreakRecover : IModularConsequence
 {
 	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
 	{
-		var modelList = modular.GetTargetModelList(circles[0]);
+		List<BattleUnitModel>  modelList = modular.GetTargetModelList(circles[0]);
 		bool force = circles.Length > 1 && (circles[1] == "force" || circles[1] == "forced");
 		BATTLE_EVENT_TIMING timing = modular.battleTiming;
 		foreach (BattleUnitModel targetModel in modelList)
