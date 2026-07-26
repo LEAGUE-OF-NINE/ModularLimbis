5e07acf8e65691291c264dc83d005121278b12a1
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-07-26 15:58:49 -0300

    fric

--- ModularSkillScripts/Consequence/ConsequenceBreakRecover.cs
+++ ModularSkillScripts/Consequence/ConsequenceBreakRecover.cs
@@ -6,7 +6,7 @@ public class ConsequenceBreakRecover : IModularConsequence
 {
 	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
 	{
-		List<BattleUnitModel>  modelList = modular.GetTargetModelList(circles[0]);
+		List<BattleUnitModel> modelList = modular.GetTargetModelList(circles[0]);
 		bool force = circles.Length > 1 && (circles[1] == "force" || circles[1] == "forced");
 		BATTLE_EVENT_TIMING timing = modular.battleTiming;
 		foreach (BattleUnitModel targetModel in modelList)
