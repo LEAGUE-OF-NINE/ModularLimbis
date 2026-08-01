4490597ad922f73ed8efd6263908ecfc99f24869
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-08-01 13:52:40 -0300

    bufcheck now supports sublist and category

--- ModularSkillScripts/Acquirer/AcquirerBufCheck.cs
+++ ModularSkillScripts/Acquirer/AcquirerBufCheck.cs
@@ -1,3 +1,4 @@
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using Lethe.Patches;
 
 namespace ModularSkillScripts.Acquirer;
@@ -8,37 +9,55 @@ public class AcquirerBufCheck : IModularAcquirer
 	{
 		BattleUnitModel targetModel = modular.GetTargetModel(circles[0]);
 		if (targetModel == null) return -1;
-
+		int circles_length = circles.Length;
 		BUFF_UNIQUE_KEYWORD buf_keyword = CustomBuffs.ParseBuffUniqueKeyword(circles[1]);
 		BattleUnitBuffManager instance = Singleton<BattleUnitBuffManager>.Instance;
 		BuffDetail bufDetail = targetModel._buffDetail;
-		//BuffModel buf = bufDetail.FindActivatedBuff(buf_keyword, false);
 		int stack = 0;
 		int turn = 0;
-		int turnConsumed = 0;
-		int stackConsumed = 0;
-		bool usedcheck = circles.Length >= 4;
-		if (usedcheck)
+		
+		bool check_more = circles_length > 3;
+		if (check_more)
 		{
-			stack = 0;
+			string circle_3 = circles[3];
+			if (circle_3 == "main") {
+				stack = bufDetail.GetActivatedBuffStack(buf_keyword, true);
+				turn = bufDetail.GetActivatedBuffTurn(buf_keyword, true);
+			} else if (circle_3 == "mainandsub") {
+				stack = bufDetail.GetActivatedBuffStack(buf_keyword, false);
+				turn = bufDetail.GetActivatedBuffTurn(buf_keyword, false);
+			} else if (circle_3 == "mainandsubandcategory") {
+				BUFF_CATEGORY_KEYWORD category = BUFF_CATEGORY_KEYWORD.NONE;
+				Il2CppSystem.Enum.TryParse<BUFF_CATEGORY_KEYWORD>(circles[4], out category);
+				foreach (BuffModel buf in targetModel.GetActivatedBuffModels())
+				{
+					if (buf.HasCategoryKeyword(category) || buf.IsKeyword(buf_keyword))
+					{
+						stack += buf.GetStack(0);
+						turn += buf.GetTurn(0);
+					}
+				}
+			} else if (circle_3 == "onlycategory") {
+				BUFF_CATEGORY_KEYWORD category = BUFF_CATEGORY_KEYWORD.NONE;
+				Il2CppSystem.Enum.TryParse<BUFF_CATEGORY_KEYWORD>(circles[4], out category);
+				stack = bufDetail.GetActivatedBuffStack(category, new Il2CppStructArray<BUFF_UNIQUE_KEYWORD>([BUFF_UNIQUE_KEYWORD.None]));
+				turn = bufDetail.GetActivatedBuffTurn(category, new Il2CppStructArray<BUFF_UNIQUE_KEYWORD>([BUFF_UNIQUE_KEYWORD.None]));
+			}
 		}
 		else
 		{
 			stack = bufDetail.GetActivatedBuffStack(buf_keyword, false);
 			turn = bufDetail.GetActivatedBuffTurn(buf_keyword, false);
-			turnConsumed = instance.GetUsedBuffTurn(targetModel.InstanceID, buf_keyword);
-			stackConsumed = instance.GetUsedBuffStack(targetModel.InstanceID, buf_keyword);
 		}
-
 
 		return circles[2] switch
 		{
 			"turn" => turn,
 			"+" => stack + turn,
 			"*" => stack * turn,
-			"consumed" => turnConsumed, // keep compatibility
-			"turnConsumed" => turnConsumed,
-			"stackConsumed" => stackConsumed,
+			"consumed" => instance.GetUsedBuffTurn(targetModel.InstanceID, buf_keyword), // keep compatibility
+			"turnConsumed" => instance.GetUsedBuffTurn(targetModel.InstanceID, buf_keyword),
+			"stackConsumed" => instance.GetUsedBuffStack(targetModel.InstanceID, buf_keyword),
  			_ => stack
 		};
 	}
