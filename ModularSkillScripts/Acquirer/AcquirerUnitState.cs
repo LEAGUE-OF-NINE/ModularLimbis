f56360ec994508c44a154450ba59f06a82c32212
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-08-01 13:16:20 -0300

    unitstate stagger

--- ModularSkillScripts/Acquirer/AcquirerUnitState.cs
+++ ModularSkillScripts/Acquirer/AcquirerUnitState.cs
@@ -4,8 +4,16 @@ public class AcquirerUnitState : IModularAcquirer
 {
 	public int ExecuteAcquirer(ModularSA modular, string section, string circledSection, string[] circles)
 	{
-		BattleUnitModel targetModel = modular.GetTargetModel(circledSection);
+		BattleUnitModel targetModel = modular.GetTargetModel(circles[0]);
 		if (targetModel == null) return -1;
+		
+		if (circles.Length > 1)
+		{
+			if (targetModel.IsDead()) return -1;
+			if (targetModel.IsBreak()) return targetModel.IsForcelyBreak() ? 2 : 1;
+			return 0;
+		}
+		
 		if (targetModel.IsDead()) return 0;
 		if (targetModel.IsBreak()) return 2;
 		var abnoPart = targetModel.TryCast<BattleUnitModel_Abnormality_Part>();
