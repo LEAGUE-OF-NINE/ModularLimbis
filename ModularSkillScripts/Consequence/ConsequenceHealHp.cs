6a0701cc4827e6455c184b383fa100541c8979cc
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-08-07 15:35:17 -0300

    healhp +core

--- ModularSkillScripts/Consequence/ConsequenceHealHp.cs
+++ ModularSkillScripts/Consequence/ConsequenceHealHp.cs
@@ -13,33 +13,56 @@ public class ConsequenceHealHp : IModularConsequence
 		bool percentageheal = circle_1.Last() == '%';
 		if (percentageheal) circle_1 = circle_1.Remove(circle_1.Length - 1);
 		int amount = modular.GetNumFromParamString(circle_1);
+		
+		bool plusCore = circles.Length > 2 && circles[2] == "+core";
 
+		BattleUnitModel healsource = modular.modsa_unitModel;
+		BattleActionModel actionOrNull = modular.modsa_selfAction;
+		
 		foreach (BattleUnitModel targetModel in modelList)
 		{
 			int finalAmount = amount;
 			if (percentageheal) finalAmount = targetModel.MaxHp * finalAmount / 100;
-			if (finalAmount < 0)
+			if (finalAmount < 0) {
 				targetModel.TakeAbsHpDamage(null, finalAmount * -1, out _, out _, modular.battleTiming, DAMAGE_SOURCE_TYPE.SKILL);
-			else
-			{
+				if (plusCore) {
+					BattleUnitModel_Abnormality_Part target_part = targetModel.TryCast<BattleUnitModel_Abnormality_Part>();
+					BattleUnitModel target_core = target_part?._abnormality;
+					if (target_core != null && target_core != targetModel) targetModel.TakeAbsHpDamage(null, finalAmount * -1, out _, out _, modular.battleTiming, DAMAGE_SOURCE_TYPE.SKILL);
+				}
+			} else {
 				switch (modular.abilityMode)
 				{
-					case 2:
-						modular.dummyPassiveAbility.AddTriggeredData_HpHeal(finalAmount, targetModel.InstanceID, modular.battleTiming);
-						//dummyPassiveAbility.HealTargetHp(modsa_unitModel, modsa_selfAction, targetModel, finalAmount, modular.battleTiming, finalAmount);
-						targetModel.TryRecoverHp(modular.modsa_unitModel, null, finalAmount, ABILITY_SOURCE_TYPE.PASSIVE, modular.battleTiming,
-							out _);
-						break;
-					case 1:
-						modular.dummyCoinAbility.AddTriggeredData_HpHeal(finalAmount, targetModel.InstanceID, modular.battleTiming);
-						//dummyCoinAbility.HealTargetHp(modular.modsa_unitModel, modsa_selfAction, targetModel, finalAmount, modular.battleTiming, finalAmount);
-						targetModel.TryRecoverHp(modular.modsa_unitModel, null, finalAmount, ABILITY_SOURCE_TYPE.SKILL, modular.battleTiming, out _);
-						break;
-					default:
-						modular.dummySkillAbility.AddTriggeredData_HpHeal(finalAmount, targetModel.InstanceID, modular.battleTiming);
-						//dummySkillAbility.HealTargetHp(modular.modsa_unitModel, modsa_selfAction, targetModel, finalAmount, modular.battleTiming, finalAmount);
-						targetModel.TryRecoverHp(modular.modsa_unitModel, null, finalAmount, ABILITY_SOURCE_TYPE.SKILL, modular.battleTiming, out _);
-						break;
+					case 2: {
+						modular.dummyPassiveAbility.HealTargetHp(healsource, actionOrNull, targetModel, finalAmount, modular.battleTiming, out _);
+						if (plusCore) {
+							BattleUnitModel_Abnormality_Part target_part = targetModel.TryCast<BattleUnitModel_Abnormality_Part>();
+							BattleUnitModel target_core = target_part?._abnormality;
+							if (target_core != null && target_core != targetModel) {
+								modular.dummyPassiveAbility.HealTargetHp(healsource, actionOrNull, target_core, finalAmount, modular.battleTiming, out _);
+							}
+						}
+					} break;
+					case 1: {
+						modular.dummyCoinAbility.HealTargetHp(healsource, actionOrNull, targetModel, finalAmount, modular.battleTiming, out _);
+						if (plusCore) {
+							BattleUnitModel_Abnormality_Part target_part = targetModel.TryCast<BattleUnitModel_Abnormality_Part>();
+							BattleUnitModel target_core = target_part?._abnormality;
+							if (target_core != null && target_core != targetModel) {
+								modular.dummyCoinAbility.HealTargetHp(healsource, actionOrNull, target_core, finalAmount, modular.battleTiming, out _);
+							}
+						}
+					} break;
+					default: {
+						modular.dummySkillAbility.HealTargetHp(healsource, actionOrNull, targetModel, finalAmount, modular.battleTiming, out _);
+						if (plusCore) {
+							BattleUnitModel_Abnormality_Part target_part = targetModel.TryCast<BattleUnitModel_Abnormality_Part>();
+							BattleUnitModel target_core = target_part?._abnormality;
+							if (target_core != null && target_core != targetModel) {
+								modular.dummySkillAbility.HealTargetHp(healsource, actionOrNull, target_core, finalAmount, modular.battleTiming, out _);
+							}
+						}
+					} break;
 				}
 			}
 		}
