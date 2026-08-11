8a5170e2773de31f39630c3d676ea4c780016a3c
Not-Nova-Shane <ezpz12453@gmail.com>
2026-08-10 23:02:39 -0400

    vers bump

--- ModularSkillScripts/MainClass.cs
+++ ModularSkillScripts/MainClass.cs
@@ -623,7 +623,7 @@ public class MainClass : BasePlugin
 	public static bool logEnabled = false; // for useless logs
 
 	public const string NAME = "ModularSkillScripts";
-	public const string VERSION = "5.1.0";
+	public const string VERSION = "5.1.1";
 	public const string AUTHOR = "GlitchGames";
 	public const string GUID = $"{AUTHOR}.{NAME}";
 
