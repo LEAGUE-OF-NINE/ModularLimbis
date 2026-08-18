b547af6819c996458d91d834557f6267eb2c19ec
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-08-18 18:24:52 -0300

    More code for AcquirerIsBaseSkill

--- ModularSkillScripts/Acquirer/AcquirerIsBaseSkill.cs
+++ ModularSkillScripts/Acquirer/AcquirerIsBaseSkill.cs
@@ -1,23 +1,35 @@
+using System;
+
 namespace ModularSkillScripts.Acquirer;
 
 public class AcquirerIsBaseSkill : IModularAcquirer
 {
 	public int ExecuteAcquirer(ModularSA modular, string section, string circledSection, string[] circles)
 	{
+		string unit_s = circles[0];
+		string skill_s = circles[1];
+		
+		BattleUnitModel targetModel = unit_s == "This" ? modular.modsa_unitModel : modular.GetTargetModel(unit_s);
+		if (targetModel == null) return -1;
+		
 		int skillID = -1;
-		if (modular.modsa_skillModel != null) skillID = modular.modsa_skillModel.GetID();
-		else if (modular.modsa_selfAction != null) skillID = modular.modsa_selfAction.Skill.GetID();
-
-		if (skillID != -1) 
-		{
-			foreach (SkillModel skill in modular.modsa_unitModel.GetSkillList())
-			{
-				if (skill.GetID() == skillID)
-				{
-					return 1;
-				}
-			}
+		if (skill_s == "This") {
+			SkillModel skill = modular.modsa_skillModel;
+			if (skill != null) skillID = skill.GetID();
 		}
-		return 0;
+		else skillID = modular.GetNumFromParamString(skill_s);
+
+		if (skillID < 1) return -1;
+
+		switch (circles[2]) {
+			case "CheckSkillList": {
+				foreach (SkillModel skill_x in modular.modsa_unitModel.GetSkillList()) {
+					if (skill_x.GetID() == skillID) return 1;
+				}
+			} return 0;
+			case "IsDefaultSkill": return targetModel.HasSkillAsDefaultSkillIncludeDefenseSkill(modular.modsa_skillModel) ? 1 : 0;
+		}
+		
+		return -1;
 	}
 }
